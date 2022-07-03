52c9252e5c99f1bbc5a556a1090e7e9a37923a00
josephat9 <josephatmutale94@gmail.com>
2022-07-03 14:26:48 -0700

    hello betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - Entry point
+ * Return: success always 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\ni")
