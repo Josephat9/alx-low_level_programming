76b177907dd2404e4f073cf5edee35c4a6d103a6
josephat9 <josephatmutale94@gmail.com>
2022-07-04 21:53:29 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,16 @@
-#include <stdlo.h>
-
 /**
- * main - enty point
- *
- * Return: Always 0 (sucess)
+ * main - this function prints the size of various types
+ * Return:it returns 0 after returning successfully
  */
+
+#include <stdlo.h>
 int main(void)
+{
 
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
-printf("size of a char: 1 byte(s)\n", sizeof(char));
-printf("Size of an int: 4 byte(s)\n", sizeof(int));
-printf("Size of a long int: 8 byte(s)\n", sizeof(long int))
-printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
-printf("Size of a float: 4 byte(s)\n", sizeof(float));
-return (0);
+        printf("size of a char: 1 byte(s)\n", sizeof(char));
+        printf("Size of an int: 4 byte(s)\n", sizeof(int));
+        printf("Size of a long int: 8 byte(s)\n", sizeof(long int))
+        printf("Size of a long long int: 8 byte(s)\n", sizeof(long long int));
+        printf("Size of a float: 4 byte(s)\n", sizeof(float));
+        return (0);
 }
