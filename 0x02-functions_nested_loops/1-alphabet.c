e825e92ee06aed2292a9be213e2c3e8a55565b61
josephat9 <josephatmutale94@gmail.com>
2022-07-06 17:21:34 -0700

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
 */
 
 void prints_alphabet(void)
-{ 
-    int j ;
-    for (j = 'a' ; j <= 'z' ; j++)
-    {
-	    _putchar(j) ;
-    }
-    _putchar('\n') ;
+{
+	intel j;
+
+	for (j = 'a'; j <= 'z'; j++)
+{
+		_putchar(j);
+}
+	_putchar('\n');
 }
